b5ce915cf95de8f5982927dbf53d2f6ee156c421
Chen, Zhenhuan <zhenhuan.chen@intel.com>
2023-04-22 22:53:24 -0700

    add fp16 version onednn matmul with template

--- intel_extension_for_deepspeed/op_builder/csrc/includes/onednn_wrappers.hpp
+++ intel_extension_for_deepspeed/op_builder/csrc/includes/onednn_wrappers.hpp
@@ -1,17 +1,7 @@
 #pragma once
-#if __has_include(<sycl/sycl.hpp>)
-#include <sycl/sycl.hpp>
-using namespace sycl;
-#elif __has_include(<CL/sycl.hpp>)
-#include <CL/sycl.hpp>
-using namespace cl::sycl;
-#else
-#error "Unsupported compiler"
-#endif
-#include <ext/oneapi/experimental/bfloat16.hpp>
+#include "compatible.h"
 
-using bf16 = sycl::ext::oneapi::experimental::bfloat16;
-
+template <typename T>
 int onednn_matmul_ex(sycl::queue handle,
                      bool trans_src,
                      bool trans_wgt,
@@ -20,19 +10,20 @@ int onednn_matmul_ex(sycl::queue handle,
                      int k,
                      const float alpha,
                      const float beta,
-                     const bf16* src_ptr,
-                     const bf16* wgt_ptr,
-                     bf16* dst_ptr);
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr);
 
+template <typename T>
 int onednn_batchgemm(sycl::queue handle,
                      int m,
                      int n,
                      int k,
                      const float alpha,
                      const float beta,
-                     const bf16* src_ptr,
-                     const bf16* wgt_ptr,
-                     bf16* dst_ptr,
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr,
                      bool trans_src,
                      bool trans_wgt,
                      int batch);
--- intel_extension_for_deepspeed/op_builder/csrc/transformer/onednn_wrappers.cpp
+++ intel_extension_for_deepspeed/op_builder/csrc/transformer/onednn_wrappers.cpp
@@ -1,8 +1,8 @@
 #include "onednn_wrappers.hpp"
-#include "inference_sycl_layers.h"
 #include <oneapi/dnnl/dnnl_sycl.hpp>
+#include "inference_sycl_layers.h"
 
-template <bool bmm>
+template <typename T, bool bmm>
 inline int onednn_matmul(sycl::queue handle,
                          bool trans_src,
                          bool trans_wgt,
@@ -11,9 +11,9 @@ inline int onednn_matmul(sycl::queue handle,
                          int k,
                          const float alpha,
                          const float beta,
-                         const bf16* src_ptr,
-                         const bf16* wgt_ptr,
-                         bf16* dst_ptr,
+                         const T* src_ptr,
+                         const T* wgt_ptr,
+                         T* dst_ptr,
                          int batch)
 {
     /*
@@ -27,7 +27,8 @@ inline int onednn_matmul(sycl::queue handle,
     dnnl::stream stream = dnnl::sycl_interop::make_stream(engine, handle);
 
     dnnl::memory::dims src_dims, wgt_dims, dst_dims;
-
+    constexpr auto dnnl_dtype_16 = std::is_same<T, fp16>::value ? dnnl::memory::data_type::f16
+                                                                : dnnl::memory::data_type::bf16;
     if constexpr (bmm) {
         src_dims = {batch, m, k};
         wgt_dims = {batch, k, n};
@@ -43,25 +44,23 @@ inline int onednn_matmul(sycl::queue handle,
     if constexpr (bmm) {
         src_md = dnnl::memory::desc(
             src_dims,
-            dnnl::memory::data_type::bf16,
+            dnnl_dtype_16,
             trans_src ? dnnl::memory::format_tag::acb : dnnl::memory::format_tag::abc);
         wgt_md = dnnl::memory::desc(
             wgt_dims,
-            dnnl::memory::data_type::bf16,
+            dnnl_dtype_16,
             trans_wgt ? dnnl::memory::format_tag::acb : dnnl::memory::format_tag::abc);
-        dst_md = dnnl::memory::desc(
-            dst_dims, dnnl::memory::data_type::bf16, dnnl::memory::format_tag::abc);
+        dst_md = dnnl::memory::desc(dst_dims, dnnl_dtype_16, dnnl::memory::format_tag::abc);
     } else {
         src_md = dnnl::memory::desc(
             src_dims,
-            dnnl::memory::data_type::bf16,
+            dnnl_dtype_16,
             trans_src ? dnnl::memory::format_tag::ba : dnnl::memory::format_tag::ab);
         wgt_md = dnnl::memory::desc(
             wgt_dims,
-            dnnl::memory::data_type::bf16,
+            dnnl_dtype_16,
             trans_wgt ? dnnl::memory::format_tag::ba : dnnl::memory::format_tag::ab);
-        dst_md = dnnl::memory::desc(
-            dst_dims, dnnl::memory::data_type::bf16, dnnl::memory::format_tag::ab);
+        dst_md = dnnl::memory::desc(dst_dims, dnnl_dtype_16, dnnl::memory::format_tag::ab);
     }
 
     auto src_mem = dnnl::memory(src_md, engine, (void*)src_ptr);
@@ -94,6 +93,7 @@ inline int onednn_matmul(sycl::queue handle,
     /* stream.wait(); */
 }
 
+template <typename T>
 int onednn_matmul_ex(sycl::queue handle,
                      bool trans_src,
                      bool trans_wgt,
@@ -102,27 +102,78 @@ int onednn_matmul_ex(sycl::queue handle,
                      int k,
                      const float alpha,
                      const float beta,
-                     const bf16* src_ptr,
-                     const bf16* wgt_ptr,
-                     bf16* dst_ptr)
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr)
 {
-    onednn_matmul<false>(
+    onednn_matmul<T, false>(
         handle, trans_src, trans_wgt, m, n, k, alpha, beta, src_ptr, wgt_ptr, dst_ptr, 1);
 }
 
+template <typename T>
 int onednn_batchgemm(sycl::queue handle,
                      int m,
                      int n,
                      int k,
                      const float alpha,
                      const float beta,
-                     const bf16* src_ptr,
-                     const bf16* wgt_ptr,
-                     bf16* dst_ptr,
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr,
                      bool trans_src,
                      bool trans_wgt,
                      int batch)
 {
-    onednn_matmul<true>(
+    onednn_matmul<T, true>(
         handle, trans_src, trans_wgt, m, n, k, alpha, beta, src_ptr, wgt_ptr, dst_ptr, batch);
 }
+
+template int onednn_matmul_ex(sycl::queue handle,
+                              bool trans_src,
+                              bool trans_wgt,
+                              int m,
+                              int n,
+                              int k,
+                              const float alpha,
+                              const float beta,
+                              const bf16* src_ptr,
+                              const bf16* wgt_ptr,
+                              bf16* dst_ptr);
+
+template int onednn_matmul_ex(sycl::queue handle,
+                              bool trans_src,
+                              bool trans_wgt,
+                              int m,
+                              int n,
+                              int k,
+                              const float alpha,
+                              const float beta,
+                              const fp16* src_ptr,
+                              const fp16* wgt_ptr,
+                              fp16* dst_ptr);
+
+template int onednn_batchgemm(sycl::queue handle,
+                              int m,
+                              int n,
+                              int k,
+                              const float alpha,
+                              const float beta,
+                              const bf16* src_ptr,
+                              const bf16* wgt_ptr,
+                              bf16* dst_ptr,
+                              bool trans_src,
+                              bool trans_wgt,
+                              int batch);
+
+template int onednn_batchgemm(sycl::queue handle,
+                              int m,
+                              int n,
+                              int k,
+                              const float alpha,
+                              const float beta,
+                              const fp16* src_ptr,
+                              const fp16* wgt_ptr,
+                              fp16* dst_ptr,
+                              bool trans_src,
+                              bool trans_wgt,
+                              int batch);
